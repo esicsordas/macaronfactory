9f89b9786468e96db555da9c5ec55af0509033dd
Emese Csordas <64093564+esicsordas@users.noreply.github.com>
2023-09-05 11:30:29 +0200

    create backend and frontend environment

---
+++ backend/src/main/java/com/example/petProject/MacaronfactoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.petProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacaronfactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacaronfactoryApplication.class, args);
+	}
+
+}
