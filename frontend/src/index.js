f687637a2a9ffd7006e57c7062cf182c3e3c03cc
Emese Csordas <64093564+esicsordas@users.noreply.github.com>
2023-11-02 16:21:07 +0100

    create routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,33 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import MainPage from './Components/MainPage'
+import MainPage from './mainpage/MainPage'
+import MacaronList from './macaronlist/MacaronList';
+import Story from './story/Story';
+import NewMacaronForm from './newmacaron/NewMacaronForm';
+import LoginForm from './login/LoginForm';
 
 
 const router = createBrowserRouter([
 {
   path: "/",
   element: < MainPage /> ,
-  children: [
-
-  ]
+},
+{
+  path: "/all-macarons",
+  element: <MacaronList />
+},
+{
+  path: "/story",
+  element: <Story />
+},
+{
+  path: "/new-macaron",
+  element: <NewMacaronForm />
+},
+{
+  path:"/login",
+  element: <LoginForm />
 }
 ])
 
